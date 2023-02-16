b329dc1ab39d9722846e3268a5c81f8ccf8fe74b
Collin D <97995022+cdrake1@users.noreply.github.com>
2023-02-15 22:33:19 -0500

    Update CobolCaesar.cob

--- CobolCaesar.cob
+++ CobolCaesar.cob
@@ -1,8 +1,8 @@
       * 
-      * Welcome to GDB Online.
-      * GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-      * C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-      * Code, Compile, Run and Debug online from anywhere in world.
+      * Welcome to Programming in the Past
+      * Collin Drake
+      * Creating a Caesar Cipher in COBOl
+      * Lots of problems occurring. Not sure what to do
       * 
       
         IDENTIFICATION DIVISION.
@@ -37,45 +37,4 @@
       
       
            EXIT PROGRAM.
-
-
-                 * 
-      * Welcome to GDB Online.
-      * GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-      * C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-      * Code, Compile, Run and Debug online from anywhere in world.
-      * 
-      
-        IDENTIFICATION DIVISION.
-        PROGRAM-ID. CAESARSALAD.
-        ENVIRONMENT DIVISION.
-        DATA DIVISION.
-        WORKING-STORAGE SECTION.
-        01 test1 PIC X(20) VALUE "BuGs BunnY".
-        01 test2 PIC X(25) VALUE "Spaghetti and Meatballs".
-        01 test3 PIC X(20) VALUE "Hello World".
-        01 shiftcypher PIC 9 VALUE 7.
-        01 SHIFTED-TEXT OCCURS 26 TIMES PIC X(80).
-        PROCEDURE DIVISION.
-      
-          
-        FUNCTION encrypt
-            USING tes, shift
-            
-            LOCAL VARIABLES
-                i PIC 9(4) VALUE 1.
-            END-LOCAL
-            
-            PERFORM VARYING i FROM 1 BY 1 
-                    UNTIL i > LENGTH of test1
-                DISPLAY "HELLO"
-            END PERFORM
-        END FUNCTION
-        
-        MAIN PROCEDURE.
-            encrypt(test1, shiftcypher)
-            
-            STOP RUN.
-      
-      
         
