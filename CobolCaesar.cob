3bd75003bedb5b82736cda730b8186b37b9b4122
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-02-16 15:11:27 -0500

    done finally

--- CobolCaesar.cob
+++ CobolCaesar.cob
@@ -10,9 +10,9 @@
         
         DATA DIVISION.
            WORKING-STORAGE SECTION.
-           01 test1 PIC X(20) VALUE 'Bugs Bunny'.
-           01 test2 PIC X(20) VALUE 'Hello World'.
-           01 test3 PIC X(20) VALUE 'Spaghetti and Meatballs'.
+           01 test1 PIC X(25) VALUE 'Bugs Bunny'.
+           01 test2 PIC X(25) VALUE 'Hello World'.
+           01 test3 PIC X(25) VALUE 'Spaghetti and Meatballs'.
            01 shiftcipher PIC 99 VALUE 7.
         
         PROCEDURE DIVISION.
@@ -71,8 +71,59 @@
                 END-IF
            END-PERFORM.
            DISPLAY 'Your string decrypted: ', tempd.
+           CALL 'SOLVE' USING tempd.
+           
 
         END PROGRAM DECRYPT.
+        
+        
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. SOLVE IS COMMON.
+    
+        DATA DIVISION.
+        WORKING-STORAGE SECTION.
+         01 Z Pic 99(01).
+           LINKAGE SECTION.
+           01 temps PIC X(20).
+        
+        PROCEDURE DIVISION USING temps.
+           
+           PERFORM VARYING Z FROM 1 BY 1 UNTIL Z > 26
+                CALL 'SOLVE2' USING temps
+           END-PERFORM.
+
+        END PROGRAM SOLVE.
+        
+        
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. SOLVE2 IS COMMON.
+    
+        DATA DIVISION.
+        WORKING-STORAGE SECTION.
+         01 A Pic 99(01).
+         01 shiftl PIC 99 VALUE 1.
+           LINKAGE SECTION.
+           01 templ PIC X(20).
+        
+        PROCEDURE DIVISION USING templ.
+           
+           PERFORM VARYING A FROM 1 BY 1 UNTIL A > FUNCTION 
+           LENGTH(templ)
+                IF FUNCTION ORD (templ(A:1)) >= 97 AND 
+                FUNCTION ORD (templ(A:1)) <= 122 THEN
+                    MOVE FUNCTION CHAR( FUNCTION MOD(FUNCTION ORD
+                    (templ(A:1)) - 97 + shiftl, 26) + 97) TO templ(A:1)
+                END-IF
+           END-PERFORM.
+           DISPLAY 'solved: ', templ.
+
+        END PROGRAM SOLVE2.
+        
+        
+        
+   
+        
+        
         
         
        
