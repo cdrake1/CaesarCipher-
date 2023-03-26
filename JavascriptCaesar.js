984df4e6504ddab6aaf66e98c6056733c26790fe
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-03-25 20:15:38 -0400

    java almost done. need solve to complete

--- JavascriptCaesar.js
+++ JavascriptCaesar.js
@@ -3,10 +3,11 @@ function CaesarCipher()
 {
 	var testString = "Bugs Bunny";
 	var shifta = 7;
-	testString = testString.toLowerCase();  //make string lower case
-	print(testString);
-	var encryptOne = testString.split("").map(ascii);
 	var i = 0;
+	
+	testString = testString.toLowerCase();  //make string lower case
+	print("Original String: ", testString);
+	var encryptOne = testString.split("").map(ascii); //make string into array of ascii
 	Encrypt(encryptOne, shifta, testString, i); //call encrypt function
 
 }
@@ -15,23 +16,24 @@ function Encrypt(testOne, shiftb, originalString, j)
 {
     var L = originalString.length
     
-    if(testOne[j] >= 97 && testOne[j] <= 122)
+    if(testOne[j] >= 97 && testOne[j] <= 122) //check if between 97 and 122
     {
-        testOne[j] = ((testOne[j] - 97 + shiftb) % 26) + 97; 
+        testOne[j] = ((testOne[j] - 97 + shiftb) % 26) + 97; //shift and wrap alphabet
     }
     
-    j = j+1;
+    j = j+1; //increase counter
     
     if(j < L)
     {
-        Encrypt(testOne, shiftb, originalString, j);
+        Encrypt(testOne, shiftb, originalString, j);  //recursively call encrypt
     }
     else
     {
-        var encryptedOne = testOne.map(fix).join("");
-        print(encryptedOne);
+        var encryptedOne = testOne.map(fix).join("");  //set it back to string
+        print("Encrypted String: ", encryptedOne);
         j = 0;
-        Decrypt(testOne, shiftb, originalString, j, L);
+        Decrypt(testOne, shiftb, originalString, j, L);  //call decrypt
+        Solve(testOne, 1, j, L, 1);
     }
     
     
@@ -42,31 +44,61 @@ function Decrypt(testTwo, shiftc, StringLe, k, Le)
     if (testTwo[k] >= 97 && testTwo[k] <= 122) 
     {
         var check = (testTwo[k] - 97 - shiftc) % 26;
-        if (check < 0) 
+        
+        if (check < 0) //javascript has this weird thing where if you're modding a negative it just sets it to itself so this is a fix for that
         {
             check += 26;
         }
         testTwo[k] = check + 97; 
+        
     }
 
     
-    k = k+1;
+    k = k+1;  //increase counter
     
     if(k < Le)
     {
-        Decrypt(testTwo, shiftc, StringLe, k, Le);
+        Decrypt(testTwo, shiftc, StringLe, k, Le);  //recursively call itself
     }
     else
     {
-        var decrypted = testTwo.map(fix).join("");
-        print(decrypted);
-        
+        var decrypted = testTwo.map(fix).join("");  //set back to string
+        print("Decrypted String: ", decrypted);
     }
     
     
 }
 
-function Solve()
+
+function Solve(final, maxShift, l, len, shiftd)
+{
+    final = final;
+    if(maxShift <= 26)  // modified condition
+    {
+        print(maxShift);
+        if(final[l] >= 97 && final[l] <= 122) //check if between 97 and 122
+        {
+            final[l] = ((final[l] - 97 + shiftd) % 26) + 97; //shift and wrap alphabet
+        }
+    
+        l += 1; //increase counter
+    
+        if(l < len)
+        {
+            Solve(final, maxShift, l, len, shiftd);  //recursively call encrypt
+        }
+        else
+        {
+            var solved = final.map(fix).join("");
+            print(solved);
+        }
+        
+    }   
+    shiftd += 1;
+    maxShift += 1;
+}
+
+
 
 function ascii (a) //returns ascii value of character at spot a
 { return a.charCodeAt(0); }
