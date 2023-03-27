15541c627be5226981a5bed07c0d0372087db300
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2023-03-26 21:22:14 -0400

    minor changers

--- ScalaPt2.scala
+++ ScalaPt2.scala
@@ -3,25 +3,39 @@ object CaesarCipher
    def main(args: Array[String]) 
    {
       var testString = "Bugs Bunny"
-      testString = testString.toLowerCase() //make string lowercase
-      println(testString)
       var shifta = 7
+      var i = 0
+      testString = testString.toLowerCase() //make string lowercase
+      
+      println(testString)
       println(shifta)
       
-      encrypt(testString, shifta) //call encrypt function
+      encrypt(testString, shifta, i) //call encrypt function
 
    }
    
-   def encrypt(testOne: String, shiftb: Int)
+   def encrypt(testOne: String, shiftb: Int, j: Int)
    {
        
-       var x = testOne.split("").map(ascii)
+      var x = testOne.toCharArray.map(ascii)
+      println(x(j))
+      j += 1
+      
+      encrypt(testOne, shiftb, j)
+      
+      /*
+      var x = testOne.charAt(0)
+      print(x)
+      var y = x.toInt
+      println(y)
+      */
+        
        
        
    }
    
-   def ascii(a: String)
+   def ascii(a: Char): Int =
    {
-       return a.toInt()
+       return a.toInt
    }
 }
