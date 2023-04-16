a405329197aa6d6e7c260602bcc16ff269b7cc86
Collin D <97995022+cdrake1@users.noreply.github.com>
2023-04-16 16:01:01 -0400

    Update ErlangCaesar.erl

--- ErlangCaesar.erl
+++ ErlangCaesar.erl
@@ -1,9 +1,16 @@
 -module(prog).
--export([main/0]).
+-export([encrypt/2, main/0]).
+
+encrypt(AsciiList, ShiftA) ->
+	io:fwrite("~p~n", [AsciiList]),
+	io:fwrite("~p~n", [ShiftA]).
+	
 
 main() ->
 	TestString = "Bugs Bunny",
 	ShiftAmount = 7,
 	TestLower = string:to_lower(TestString),
-	CharList = re:split(TestLower, ""),
-	io:fwrite("~p~n", [CharList]).
+	IntList = lists:map(fun erlang:integer_to_list/1, TestLower),
+	%% CharList = re:split(TestLower, ""),
+	%%io:fwrite("~p~n", [IntList]),
+	encrypt(IntList, ShiftAmount).
