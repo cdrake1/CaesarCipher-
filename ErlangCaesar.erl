f88475f43767490c803f283cf00865abc782accd
Collin D <97995022+cdrake1@users.noreply.github.com>
2023-04-20 16:04:13 -0400

    Update ErlangCaesar.erl

--- ErlangCaesar.erl
+++ ErlangCaesar.erl
@@ -1,63 +1,20 @@
 -module(main).
 -export([start/0]).
 
-%%shiftList(ThisChar, ShiftB) ->
-  %if
-    %ThisChar == 32 ->
+shiftList(ThisChar, ShiftC) ->
+  if
+    ThisChar == " " -> " ";
+    
+    ThisChar >= 97 && ThisChar <= 122 ->
+      Shifted = ThisChar + ShiftC;
       
-  %true ->
-   %   ww
-      
-encrypt(ThatList, ShiftB, CountT) ->
-
-  
-  
-	if 
-		CountT < length(ThatList) -> 
-      io:fwrite("~w~n", [lists:nth(CountT, ThatList)]),
-			encrypt(ThatList, ShiftB, CountT + 1);
-		true ->
-			ok
-	end.
-
-decrypt(ThisList, ShiftA, CountO) ->
-  
-	if 
-		CountO < length(ThisList) -> 
-      io:fwrite("~w~n", [lists:nth(CountO, ThisList)]),
-			encrypt(ThisList, ShiftA, CountO + 1);
-		true ->
-			ok
-	end.
-
-start() ->
-	TestString = "Bugs Bunny",
-	ShiftPos = 7,
-  ShiftNeg = -7,
-	Test = 1,
-	TestLower = string:to_lower(TestString),
-	CharList = re:split(TestLower, ""),
-	Encrypted = encrypt(CharList, ShiftPos, Test),
-  Decrypted = decrypt(CharList, ShiftNeg, Test).
-
-
-%%%%%
-
-
--module(main).
--export([start/0]).
-
-%%shiftList(ThisChar, ShiftB) ->
-  %if
-    %ThisChar == 32 ->
-      
-  %true ->
-   %   ww
+    true -> ThisChar
+  end.
       
 encrypt(ThatList, ShiftB, CountT) ->
 
-  io:fwrite("~p~n", [ShiftB]),
-  lists:map(fun(I) -> I + ShiftB end, ThatList).
+  Shifts = lists:map(fun(I) -> shiftList(I , ShiftB) end, ThatList),
+  io:format("~s~n", [Shifts]).
 
 	
 
