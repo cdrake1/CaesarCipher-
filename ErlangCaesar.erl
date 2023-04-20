5be9377122d1c99855b877e39583fa910d130eff
Collin D <97995022+cdrake1@users.noreply.github.com>
2023-04-20 15:35:08 -0400

    Update ErlangCaesar.erl

--- ErlangCaesar.erl
+++ ErlangCaesar.erl
@@ -39,3 +39,42 @@ start() ->
 	CharList = re:split(TestLower, ""),
 	Encrypted = encrypt(CharList, ShiftPos, Test),
   Decrypted = decrypt(CharList, ShiftNeg, Test).
+
+
+%%%%%
+
+
+-module(main).
+-export([start/0]).
+
+%%shiftList(ThisChar, ShiftB) ->
+  %if
+    %ThisChar == 32 ->
+      
+  %true ->
+   %   ww
+      
+encrypt(ThatList, ShiftB, CountT) ->
+
+  io:fwrite("~p~n", [ShiftB]),
+  lists:map(fun(I) -> I + ShiftB end, ThatList).
+
+	
+
+decrypt(ThisList, ShiftA, CountO) ->
+  
+	if 
+		CountO < length(ThisList) -> 
+      io:fwrite("~w~n", [lists:nth(CountO, ThisList)]),
+			encrypt(ThisList, ShiftA, CountO + 1);
+		true ->
+			ok
+	end.
+
+start() ->
+	TestString = "Bugs Bunny",
+	Test = 1,
+	TestLower = string:to_lower(TestString),
+	CharList = re:split(TestLower, ""),
+	Encrypted = encrypt(CharList, 7, Test),
+  Decrypted = decrypt(CharList,-7, Test).
