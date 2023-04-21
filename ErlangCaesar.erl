258d970f736e9c27d0188f78bb82b591cfdd7ef0
Collin D <97995022+cdrake1@users.noreply.github.com>
2023-04-20 20:44:16 -0400

    Update ErlangCaesar.erl

--- ErlangCaesar.erl
+++ ErlangCaesar.erl
@@ -3,29 +3,45 @@
 
 shiftList(A, ShiftA) ->
   if
-    (A >= 97) and (A =< 122) ->  %check if lowercase
-      (A - 97 + ShiftA) rem 26 + 97;  %shift and wrap alphabet
-    (A < 0) -> 
-      A + 26;
-    (A == 32) ->  %check if its a space
+    (A >= 97) and (A =< 122) -> %check if lowercase
+      Shifted = (A - 97 + ShiftA) rem 26, %shift and wrap alphabet
+      if
+        (Shifted < 0) -> Shifted + 26; %check if shifted variable drops below 0
+        true -> Shifted
+      end + 97;
+    (A == 32) -> %check if its a space
       A;
-    true ->  %if not lowercase or space return NUL/Undefined
+    true -> %if not lowercase or space return NUL/Undefined
       undefined
   end.
       
 encrypt(TestOne, ShiftB) ->
 
-  EncryptString = lists:map(fun(I) -> shiftList(I , ShiftB) end, TestOne), %map each character to shiftList function
+  EncryptString = lists:map(fun(I) -> shiftList(I , ShiftB) end, TestOne), % map each character to shiftList function
   io:format("~s~n", [EncryptString]),
   EncryptString.
 
 decrypt(TestTwo, ShiftC) ->
-
-  DecryptString = encrypt(TestTwo, ShiftC),  %call encrypt with reverse shift
+%working but not correct
+  DecryptString = encrypt(TestTwo, ShiftC),
   DecryptString.
+
+solve(Original, ShiftD, Count) ->
+% not working
+  if 
+    Count < 26 ->
+      Solved = Original,
+      Solve = encrypt(Solved, Count),
+      io:format("~s~n", [Solve]),
+      CountTemp = Count + 1,
+      solve(Solve, ShiftD, CountTemp);
+    true ->
+      undefined
+  end.
 
 start() ->
 	TestString = "Bugs Bunny",
-	TestLower = string:to_lower(TestString), %make lower
-	Encrypted = encrypt(TestLower, 7), %pass to function
-  Decrypted = decrypt(Encrypted,-7). %pass to function
+	TestLower = string:to_lower(TestString), % make lower
+	Encrypted = encrypt(TestLower, 7), % pass to function
+	Decrypted = decrypt(Encrypted,-7), % pass to function
+	solve(Decrypted, 1, 26).
